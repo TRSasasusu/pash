641b1547339a6177374373b97aa1332751272f7d
Kaito Kishi <trsasasusu@gmail.com>
2018-06-09 01:05:41 +0900

    Run ls from python shell.

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+
+from .pash_func import *
---
+++ pash_func.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import subprocess
+
+
+def pash(command, get_return=False):
+    result = subprocess.run(command.split(' '), stdout=subprocess.PIPE).stdout.decode('utf8')
+    if get_return:
+        return result
+    print(result)
