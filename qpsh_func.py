2a005cb7d1f35aeadfd6c5c6e53e9da65df2e8f0
Kaito Kishi <trsasasusu@gmail.com>
2018-06-09 03:11:38 +0900

    Get shell commands.

--- qpsh_func.py
+++ qpsh_func.py
@@ -1,6 +1,15 @@
 # coding: utf-8
 
 import subprocess
+from .find_exe import find_exe
+
+
+class Command:
+    def __init__(self, command):
+        self.command = command
+
+    def __repr__(self):
+        return qpsh(self.command, True)
 
 
 def qpsh(command, get_return=False):
@@ -8,3 +17,13 @@ def qpsh(command, get_return=False):
     if get_return:
         return result
     print(result)
+
+
+for command in find_exe():
+    try:
+        exec('{command} = Command("{command}")'.format(command=command))
+    except:
+        print(command)
+        import sys
+        print(sys.exc_info())
+        sys.exit()
